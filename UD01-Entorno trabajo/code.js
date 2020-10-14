08ba3cdda00e45bdd8b4cadbf1b197a43f7aad4a
Ana Cabanach <stefffy2014@gmail.com>
2020-10-14 18:13:44 +0200

    c

--- UD01-Entorno trabajo/code.js
+++ UD01-Entorno trabajo/code.js
@@ -3,15 +3,20 @@ tablero[0] = ["_","_","_"];
 tablero[1] = ["_","o","_"];
 tablero[2] = ["_","_","_"];
 
+var fin_partida =0;
+
 console.log(tablero);
 
 
-function pinto_tablero(){
+function pinto_tablero(){ // copio el array a los inputs
 
-    for(i=0;i<tablero.length; i++){
-        for(j=0;j<tablero.length;j++){
-            var id_casilla = "casilla_" + i + j;
-            document.getElementById(id_casilla).value=tablero[i][j];
+    for(var i=0; i<3 ; i++){
+        for(var j=0; j<3 ; j++){  // recorro la matriz
+            var id_casilla="casilla_"+i+j; 
+            document.getElementById(id_casilla).value=tablero[i][j]; // copio los valors a los inputs, identificandolos por id
+            if(tablero[i][j]=="o"){
+                document.getElementById(id_casilla).disabled=true; // si es jugada de la maquina, deshabilito el input
+            }
         }
     
     }
@@ -25,28 +30,71 @@ function pinto_tablero(){
 }
 pinta un barra baja*/
 
-function leoTablero(){
+
  //leer todos los valores de los inputs y meterlos en tablero
 
-for(i=0;i<tablero.length;i++){
-    for(j=0;j<tablero.length;j++){
-        var id_casilla = "casilla_" + i + j;
-        tablero[i][j] = document.getElementById(id_casilla).value;
-
-
-        if((tablero[i][j] != "O") && (tablero != "_")){
-            tablero[i][j] ="X";
-        } 
-    }   
+ function jugada(){   // cuado hago click en boton ...
+    for(var i=0; i<3 ; i++){
+        for(var j=0; j<3 ; j++){
+            var id_casilla="casilla_"+i+j;   
+            tablero[i][j]=document.getElementById(id_casilla).value;  // copio los valores de los inputs a la matriz
+            if((tablero[i][j]!="o")&&(tablero[i][j]!="_")){  // si algun input no es ni 'o' ni '_' tiene que se una x
+                tablero[i][j]="x";
+            }
+        }
+    }
+    compruebo_filas_columnas_diagonales("x"); // compruebo si ganan las x
+    tiradaMaquina();   // turno de la maquina
+    compruebo_filas_columnas_diagonales("o"); // comprueba si ganan los o
+  
 }
+    //console.log(tablero);
+    //compruebaGanador();
+    //tiradaMaquina();
+    //compruebaGanador();
+    //var valor_01 = document.getElementById("casilla_01").value;
+    //console.log(valor_01);
+    //recoje valor de solo el 01
+    //comprobar si fila 1 2 y 3 son iguals para completar la diagonal
 
-console.log(tablero);
-compruebaGanador();
-tiradaMaquina();
-compruebaGanador();
-  //  var valor_01 = document.getElementById("casilla_01").value;
-  //  console.log(valor_01);
-  //  recoje valor de solo el 01
+function compruebo_filas_columnas_diagonales(jugador){  // compruebo si se gana o empata
+
+    if((tablero[0][0]==tablero[0][1])&&(tablero[0][1]==tablero[0][2])&&(tablero[0][2]==jugador)){ //filas
+        alert (" Ha ganado "+jugador);
+        location.reload();
+    }
+    if((tablero[1][0]==tablero[1][1])&&(tablero[1][1]==tablero[1][2])&&(tablero[1][2]==jugador)){
+        alert (" Ha ganado "+jugador);
+        location.reload();
+    }
+    if((tablero[2][0]==tablero[2][1])&&(tablero[2][1]==tablero[2][2])&&(tablero[2][2]==jugador)){
+        alert (" Ha ganado "+jugador);
+        location.reload();
+    }
+    if((tablero[0][0]==tablero[1][1])&&(tablero[1][1]==tablero[2][2])&&(tablero[2][2]==jugador)){ //diagonales
+        alert (" Ha ganado "+jugador);
+        location.reload();
+    }
+    if((tablero[0][2]==tablero[1][1])&&(tablero[1][1]==tablero[2][0])&&(tablero[2][0]==jugador)){
+        alert (" Ha ganado "+jugador);
+        location.reload();
+    }
+    if((tablero[0][0]==tablero[1][0])&&(tablero[1][0]==tablero[2][0])&&(tablero[2][0]==jugador)){ //columnas
+        alert (" Ha ganado "+jugador);
+        location.reload();
+    }
+    if((tablero[0][1]==tablero[1][1])&&(tablero[1][1]==tablero[2][1])&&(tablero[2][1]==jugador)){
+        alert (" Ha ganado "+jugador);
+        location.reload();
+    }
+    if((tablero[0][2]==tablero[1][2])&&(tablero[1][2]==tablero[2][2])&&(tablero[2][2]==jugador)){
+        alert (" Ha ganado "+jugador);
+        location.reload();
+    }
+    if(fin_partida==4){   // si la maquina lleva 4 turnos, se acaba en empate
+        alert("EMPATE!!!!");
+        location.reload();
+    } 
 }
 
 function tiradaMaquina(){
@@ -55,9 +103,10 @@ function tiradaMaquina(){
     jugada_x = Math.floor(Math.random() * 3);
     jugada_y = Math.floor(Math.random() * 3);
 
-    console.log("jugadax" + jugada_x + "jugaday" + jugada_y) ;
+    //console.log("jugadax" + jugada_x + "jugaday" + jugada_y) ;
     if(tablero[jugada_x][jugada_y] == "_"){
         tablero[jugada_x][jugada_y] = "O";
+        fin_partida++;
         pinto_tablero();
 
     }else{
@@ -65,38 +114,6 @@ function tiradaMaquina(){
         tiradaMaquina();
        
     }
-
-
     /*document.getElementById("casilla_aleatoria").value = tablero[aleatorio_o][aleatorio_o];
     tablero.replace("_", "o");*/
-}
-
-function compruebaGanador(){
-    //comprobar si fila 1 2 y 3 son iguals para completar la diagonal
-
-    if((tablero[0][0] == tablero[1][0]) && (tablero[1][0] == tablero[2][0])  && (tablero[0][0] != "_")){
-        alert("Has ganado");
-        location.reload();
-    }
-
-    if((tablero[0][1] == tablero[1][1]) && (tablero[1][1] == tablero[2][1])  && (tablero[0][1] != "_")){
-        alert("Has ganado");
-        location.reload();
-    }
-
-    if((tablero[0][2] == tablero[1][2]) && (tablero[1][2] == tablero[2][2])  && (tablero[0][2] != "_")){
-        alert("Has ganado");
-        location.reload();
-    }
-
-    if((tablero[0][0] == tablero[1][1]) && (tablero[1][1] == tablero[2][2])  && (tablero[0][0] != "_")){
-        alert("Has ganado");
-        location.reload();
-    }
-
-    if((tablero[0][2] == tablero[1][1]) && (tablero[1][1] == tablero[2][0])  && (tablero[0][2] != "_")){
-        alert("Has ganado");
-        location.reload();
-    }
-    
 }
