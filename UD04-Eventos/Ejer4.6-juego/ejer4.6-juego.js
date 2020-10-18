1fde309967e1b1c56a5ab1ef1cc0b7efc690da3b
Ana Cabanach <stefffy2014@gmail.com>
2020-10-18 14:56:02 +0200

    cambios2click

--- UD04-Eventos/Ejer4.6-juego/ejer4.6-juego.js
+++ UD04-Eventos/Ejer4.6-juego/ejer4.6-juego.js
@@ -1,14 +1,7 @@
 
 var secuencia1= new Array(9);
-//secuencia1=["_","_","_","_","_","_","_","_","_"];
 
 function pintoCirculoRojo(){
-    
-    for(var i =0;i<secuencia1.length;i++){
-    var id_circulo= "circulo_"+i;
-    console.log(id_circulo);
-    
-    }
     //console.log(secuencia1[5]);
     //console.log("circulo_" + numRandom);
 
@@ -17,10 +10,15 @@ function pintoCirculoRojo(){
     document.getElementById(cirRandom).className="objetivo"; //objetivo en el css es el que dara el fondo rojo 
 }
 
-document.getElementById(id_circulo).addEventListener("click",pintoCirculoRojo);
-
-
 function clickCirculo(){
+    for(var i =0;i<secuencia1.length;i++){
+        var id_circulo= "circulo_"+i;
+        console.log(id_circulo);
+        
+        }
+    document.getElementById(id_circulo).addEventListener("click",pintoCirculoRojo);
+
+
     pintoCirculoRojo();
 }
 
