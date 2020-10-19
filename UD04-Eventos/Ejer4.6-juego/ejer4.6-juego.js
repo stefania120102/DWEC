d20194db63dcc453e679fee073a5f0ddd9380dcb
Ana Cabanach <stefffy2014@gmail.com>
2020-10-19 11:18:45 +0200

    ultimo juego click

--- UD04-Eventos/Ejer4.6-juego/ejer4.6-juego.js
+++ UD04-Eventos/Ejer4.6-juego/ejer4.6-juego.js
@@ -2,23 +2,28 @@
 var secuencia1= new Array(9);
 
 function pintoCirculoRojo(){
-    //console.log(secuencia1[5]);
     //console.log("circulo_" + numRandom);
 
     var numRandom= Math.floor(Math.random() * 8);
     var cirRandom = "circulo_"+numRandom;
     document.getElementById(cirRandom).className="objetivo"; //objetivo en el css es el que dara el fondo rojo 
+    
+    clickCirculo();
 }
 
-function clickCirculo(){
+function clickCirculo(cirRandom){
+   
     for(var i =0;i<secuencia1.length;i++){
         var id_circulo= "circulo_"+i;
-        console.log(id_circulo);
-        
+        //console.log(id_circulo);
         }
-    document.getElementById(id_circulo).addEventListener("click",pintoCirculoRojo);
 
 
-    pintoCirculoRojo();
+        if(cirRandom==secuencia1[0]){
+        document.getElementById(id_circulo).addEventListener("click",pintoCirculoRojo);
+        //document.getElementById(id_circulo).removeAttribute("class");
+        console.log("hh");
+        }
+
 }
 
