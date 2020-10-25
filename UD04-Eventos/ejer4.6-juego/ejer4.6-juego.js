324580098c2ae790203e1f9b0137e6e6b0cb69ec
Ana Cabanach <stefffy2014@gmail.com>
2020-10-25 17:49:56 +0100

    ..

--- UD04-Eventos/ejer4.6-juego/ejer4.6-juego.js
+++ UD04-Eventos/ejer4.6-juego/ejer4.6-juego.js
@@ -1,40 +1,32 @@
 
 var secuencia1= new Array(9);
+var contador=0;
 
 function pintoCirculoRojo(){
-    //console.log("circulo_" + numRandom);
-
-    var numRandom= Math.floor(Math.random() * 8);
+    var numRandom= Math.floor(Math.random() * 9);
     var cirRandom = "circulo_"+numRandom;
-    document.getElementById(cirRandom).className="objetivo"; //objetivo en el css es el que dara el fondo rojo 
+    document.getElementById(cirRandom).className="objetivo"; //objetivo en el css es el que dara el fondo rojo
+    console.log("El circulo pintado es el n\u00FAmero " + (numRandom +1));
     
-    clickCirculo();
+    //DEBO HACER: si hace click resetea el tiempo
+
+    document.getElementById(cirRandom).addEventListener("click",pintoCirculoRojo); //cuando haga click en el circulo random pintará otro nuevo
+    contador++;
+    console.log("N\u00FAmero de rondas: " + contador);//contador de número de rondas
+    
 }
 
-function clickC(){}
+
+
+    //document.getElementById(cirRandom).className="objetivo";
+
+
+    //DEBO HACER: cuando haga click despintar el que está pintado
 
 
 
 
-function clickCirculo(cirRandom){
 
-   
-    for(var i =0;i<secuencia1.length;i++){
-        var id_circulo= "circulo_"+i;
-        
-        var hacer = document.getElementById(id_circulo).addEventListener("click",pintoCirculoRojo);
-        
-        console.log(id_circulo);
-        }
-        
-        document.getElementById(id_circulo).removeAttribute("class");
-
-
-        
-
-        
-
-}
 
 
 
--- UD04-Eventos/prueba.js
+++ UD04-Eventos/prueba.js
@@ -2,6 +2,8 @@
 var secuencia1= new Array(9);
 
 function pintoCirculoRojo(){
+    //console.log("circulo_" + numRandom);
+
     var numRandom= Math.floor(Math.random() * 8);
     var cirRandom = "circulo_"+numRandom;
     document.getElementById(cirRandom).className="objetivo"; //objetivo en el css es el que dara el fondo rojo 
@@ -9,20 +11,22 @@ function pintoCirculoRojo(){
     clickCirculo();
 }
 
-function clickC(){
-
-}
-
 function clickCirculo(cirRandom){
-
     for(var i =0;i<secuencia1.length;i++){
         var id_circulo= "circulo_"+i;
         
-        //var hacer = document.getElementById(id_circulo).addEventListener("click",pintoCirculoRojo);
+        var hacer = document.getElementById(id_circulo).addEventListener("click",pintoCirculoRojo);
         
         console.log(id_circulo);
         }
-        
         document.getElementById(id_circulo).removeAttribute("class");
-
 }
+
+/*function nuevoCirculo(){
+    var numRandom2= Math.floor(Math.random() * 9);
+    var cirRandom2 = "circulo_"+numRandom2;
+    document.getElementById(cirRandom2).className="objetivo"; //objetivo en el css es el que dara el fondo rojo
+    console.log("el circulo random nuevo es " + cirRandom2);
+    var tiempoFuera=setTimeout("alert('Se acabo el tiempo');", 5000); 
+
+}*/
