84f17981c74f2e0ca30fcfd921908aa887a70987
Ana Cabanach <stefffy2014@gmail.com>
2020-10-25 14:06:52 +0100

    orden22

--- UD04-Eventos/ejer4.6-juego/prueba.js
+++ UD04-Eventos/ejer4.6-juego/prueba.js
@@ -2,8 +2,6 @@
 var secuencia1= new Array(9);
 
 function pintoCirculoRojo(){
-    //console.log("circulo_" + numRandom);
-
     var numRandom= Math.floor(Math.random() * 8);
     var cirRandom = "circulo_"+numRandom;
     document.getElementById(cirRandom).className="objetivo"; //objetivo en el css es el que dara el fondo rojo 
@@ -11,30 +9,20 @@ function pintoCirculoRojo(){
     clickCirculo();
 }
 
-function clickC(){}
+function clickC(){
 
-
-
+}
 
 function clickCirculo(cirRandom){
 
-   
     for(var i =0;i<secuencia1.length;i++){
         var id_circulo= "circulo_"+i;
         
-        var hacer = document.getElementById(id_circulo).addEventListener("click",pintoCirculoRojo);
+        //var hacer = document.getElementById(id_circulo).addEventListener("click",pintoCirculoRojo);
         
         console.log(id_circulo);
         }
         
         document.getElementById(id_circulo).removeAttribute("class");
 
-
-        
-
-        
-
 }
-
-
-
