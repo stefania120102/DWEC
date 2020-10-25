6bccfa2db8d64c243c779f019ba75348226adb91
Ana Cabanach <stefffy2014@gmail.com>
2020-10-25 19:49:58 +0100

    ultimos cambios dia 25

--- UD04-Eventos/ejer4.6-juego/ejer4.6-juego.js
+++ UD04-Eventos/ejer4.6-juego/ejer4.6-juego.js
@@ -1,12 +1,20 @@
 var secuencia1= new Array(9);
-var contador=0;
-pintado=true;
+var contador = 0;
+//var pintado=true;
+var crono1;
+var tiempo = 0;
+var crono2;
+
+//NO PUDE DESPINTAR LOS CIRCULOS PULSADOS ':/ 
 
 function pintoCirculoRojo(){
     var numRandom= Math.floor(Math.random() * 9);
     var cirRandom = "circulo_"+numRandom;
     document.getElementById(cirRandom).className="objetivo"; //objetivo en el css es el que dara el fondo rojo
     console.log("El circulo pintado es el n\u00FAmero " + (numRandom +1));
+    window.clearTimeout(crono1);
+    crono1 = window.setTimeout(tiempoTerminado, 3000);
+
     document.getElementById(cirRandom).addEventListener("click",pintoCirculoRojo); //cuando haga click en el circulo random pintará otro nuevo
     //pintado=false;
 
@@ -21,16 +29,29 @@ function despintar(){
     //DEBO HACER: cuando haga click despintar el que está pintado y que pinte uno nuevo(pintoCirculoRojo)
 
     //if(pintado==false){
-        document.getElementById(cirRandom).removeAttribute("class");
+    //document.getElementById(cirRandom).removeAttribute("class");
     //}
 }
 
+    //Cada vez que pasen 3 segundos sin clickar en el circulo pintado mandar mensaje alerta
 function cronometro(){
-    //DEBO HACER: cada vez que pasen 3 segundos sin clickar en el circulo pintado mandar mensaje alerta
-    //DEBO HACER: cada vez que clicke resetear el tiempo
+
+    tiempo++;
+    document.getElementById("cronometro").innerHTML=tiempo;
+
+}
+
+function tiempoTerminado(){
+    
+    document.getElementsByClassName("objetivo")[0].removeEventListener("click", pintoCirculoRojo);
+    window.clearInterval(crono2);
+    alert("Has perdido, lo siento :(");
+    location.reload();
+
 }
 
 function rondasMaximas(){ //Cuando el contador llegue a 100 mandará un mensaje y recargará la página
+
     if(contador==100){
         alert("Eres un crack");
         location.reload();
