79e0b4235860ca9776c467cd946d210146bb38b5
Ana Cabanach <stefffy2014@gmail.com>
2020-11-13 15:52:56 +0100

    cambios

---
+++ PROYECTOCLIENTE/modal/modal/code.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$("#caja-modal1").hide();
+	$("#caja-modal2").hide();
+	
+
+	$("#ver-caja1").click(function(){
+		$("#caja-modal1").show();
+	})
+	$("#ver-caja2").click(function(){
+		$("#caja-modal2").show();
+	})
+	
+	$("#ocultar-caja1").click(function(){
+		$("#caja-modal1").hide();
+
+	})
+	$("#ocultar-caja2").click(function(){
+		$("#caja-modal2").hide();
+
+	})
+
+	
+
+
+});
+
+
---
+++ PROYECTOCLIENTE/proyecto/clientes.js
@@ -0,0 +1,37 @@
+function funcion_ajax(){
+    conexion = new XMLHttpRequest();
+    conexion.onreadystatechange = function(){
+            if(conexion.readyState == 4){
+                if(conexion.status == 200){
+                   var objeto_response=JSON.parse(conexion.responseText);
+                   
+                    for ( var i in objeto_response.clientes){
+                       console.log(objeto_response.clientes[i]);
+                       var tabla=document.createElement('table');
+                       document.createElement('tr');
+                       var dniC = document.createElement('td');
+                       //option_cliente.setAttribute('value',objeto_response.clientes[i].dni);
+
+                       dniC.innerHTML=objeto_response.clientes[i].dni;
+                       document.getElementById("contenido").appendChild(dniC);
+
+                       console.log(objeto_response.clientes[i]);
+                        for(x=0;x< objeto_response.length;x++){
+                            for(f=0; objeto_response[x].length;f++){
+                                console.log(objeto_response.clientes[i]);
+                            }
+                        }
+                        var nombreC = document.createElement('td');
+                        //option_cliente.setAttribute('value',objeto_response.clientes[i].dni);
+
+                         nombreC.innerHTML=objeto_response.clientes[i].nombre;
+                        document.getElementById("contenido").appendChild(nombreC);
+                   }
+                }
+            }
+    };
+
+    conexion.open('GET','clientes.json',true);
+    conexion.send(); 
+
+}
