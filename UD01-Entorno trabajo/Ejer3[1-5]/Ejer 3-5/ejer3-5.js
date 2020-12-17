9bc0ad21483d81d9cea606b6e05d64100b872b7d
Ana Cabanach <stefffy2014@gmail.com>
2020-12-17 17:39:07 +0100

    cambios

--- UD01-Entorno trabajo/Ejer3[1-5]/Ejer 3-5/ejer3-5.js
+++ UD01-Entorno trabajo/Ejer3[1-5]/Ejer 3-5/ejer3-5.js
@@ -1,14 +1,16 @@
 
 function mostrar(cadena){
+    console.log(cadena);
     cadenaPartida=cadena.split(":");
-    alert("CP "+cadenaPartida[4]);
+    alert("Nombre "+cadenaPartida[0]);
     alert("Apellidos "+cadenaPartida[1]);
+    alert("Teléfono "+cadenaPartida[2]);
     alert("Email "+cadenaPartida[3]);    
     alert("Email servidor "+cadenaPartida[3].split("@")[1]);
     
 }
 
 
-var c=prompt("Mete cadena","nonombre:apellidos:telefono:email@servidor:codigopostal");
+var c=prompt("Mete cadena","nombre:apellidos:telefono:email@servidor:codigopostal");
 
 mostrar(c);
