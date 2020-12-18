8d44922c249c3923efc7200a43a948e1b9751033
Ana Cabanach <stefffy2014@gmail.com>
2020-12-18 12:50:32 +0100

    cambios

--- UD02-Objetos predefinidos/ExA/js/liso_peliculas.js
+++ UD02-Objetos predefinidos/ExA/js/liso_peliculas.js
@@ -6,7 +6,11 @@ window.onload = function () {
     });
     $("#cerrar_modal").click(function(){
         $("#modal").hide();
-    })
+    });
+
+    $("#enviar_carta").click(function(){
+        anyadir_dato();
+    });
 }
 
 function lista_datos(){
@@ -29,10 +33,6 @@ $.ajax({
 });
 ;
 
-}
-
-function abrirModal(){
-    
 }
 
 function anyado(){
@@ -60,4 +60,36 @@ function anyado(){
             document.getElementById("total").innerHTML="TOTAL :"+total;
         }
     }
+}
+
+
+function anyadir_dato(){
+    var objeto_dato = {   //monto un objeto con los datos de la fila a insertar en la BD
+            nombre:$('#titulo').val(),
+            apellido:$('#anyo').val(),
+            edad:$('#director').val(),
+            apellido:$('#genero_peli option:selected').val(),
+            apellido:$('#actor1').val(),
+            apellido:$('#actor2').val()
+            };
+    console.log(objeto_dato)
+    
+    $.ajax({
+        url:"PHP/anyadir_dato.php",
+        type:"POST",
+        data: objeto_dato, // paso el dato por el post
+        dataType:"json",
+    }).done(function(respuesta){
+        console.log(respuesta);  // recojo la respuesta, que sera true o false
+       if(respuesta){
+            alert("Dato insertado correctamente !!!!");//si es correcta, inserto los datos en una fila nueva
+            $("#lista").append("<tr><td>"+objeto_dato.nombre+"</td><td>"+objeto_dato.apellido+"</td><td>"+objeto_dato.edad+"</td></tr>");
+        }else{ 
+            alert("Error en la insercion"); //si no es correcta no inserto nada
+        } 
+    }).fail(function( jqXHR, textStatus, errorThrown ) {
+        console.log( "La solicitud ha fallado: " +  textStatus + errorThrown);
+    });
+    ;
+
 }
