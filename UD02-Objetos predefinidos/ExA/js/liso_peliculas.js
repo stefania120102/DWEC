befe178d17fae39a1a4690e877940e64072e4249
Ana Cabanach <stefffy2014@gmail.com>
2020-12-18 13:48:22 +0100

    cambios

--- UD02-Objetos predefinidos/ExA/js/liso_peliculas.js
+++ UD02-Objetos predefinidos/ExA/js/liso_peliculas.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    document.getElementById("genero_peli").addEventListener("change",anyado);
+    //document.getElementById("genero_peli").addEventListener("change",anyado);
     lista_datos();
 	$("#abrir_modal").click(function(){
 		$("#modal").show();
@@ -10,12 +10,15 @@ window.onload = function () {
 
     $("#enviar_carta").click(function(){
         anyadir_dato();
+        var elemento=document.getElementById("peliculas");
+        elemento.submit();
     });
+    anyadir_dato();
 }
 
 function lista_datos(){
 $.ajax({
-    url:"PHP/listo_peliculas.php", // no paso ningun dato, solo recojo
+    url:"php/listo_peliculas.php", // no paso ningun dato, solo recojo
     type:"POST",
     dataType:"json",
 }).done(function(respuesta){
@@ -35,55 +38,28 @@ $.ajax({
 
 }
 
-function anyado(){
-    for(var i=0; i<options.length;i++){ //buscando el option seleccionado
-        if(options[i]){  // para recoger el atributo generoValor del elegido
-            var generoValor = parseInt(options[i].getAttribute("value")); //quiero el valor de generoValor
-            console.log(generoValor); //aqui me da drama
-            spanGnro.style.display= "inline-block"; // para poder modificar anchura
-            spanGnro.style.width= (generoValor*2)+"px"; // ancho = escaños * 2
-            spanGnro.textContent= options[i].text; //Texto
-            spanGnro.style.backgroundColor=options[i].value; //color
-            spanGnro.addEventListener("click", function(){ //gestiono evento para borrar
-                this.remove();
-                total-=generoValor;
-                document.getElementById("total").innerHTML="TOTAL :"+total;
-            } );
-            spanGnro.addEventListener("mouseenter",function(){ //cuando entra raton
-                document.getElementById("generoValor_spanGnro").innerHTML="Escaños :"+generoValor;
-            })
-            spanGnro.addEventListener("mouseleave",function(){ //cuando sale raton
-                document.getElementById("generoValor_spanGnro").innerHTML="";
-            })
-            document.getElementById("panel").appendChild(spanGnro); // lo añado
-            total += generoValor;
-            document.getElementById("total").innerHTML="TOTAL :"+total;
-        }
-    }
-}
-
-
 function anyadir_dato(){
     var objeto_dato = {   //monto un objeto con los datos de la fila a insertar en la BD
-            nombre:$('#titulo').val(),
-            apellido:$('#anyo').val(),
-            edad:$('#director').val(),
-            apellido:$('#genero_peli option:selected').val(),
-            apellido:$('#actor1').val(),
-            apellido:$('#actor2').val()
+            titulo:$('#titulo').val(),
+            anyo:$('#anyo').val(),
+            director:$('#director').val(),
+            genero:$('#genero_peli option:selected').val(),
+            actor1:$('#actor1').val(),
+            actor2:$('#actor2').val()
             };
-    console.log(objeto_dato)
+    console.log(objeto_dato);
     
     $.ajax({
-        url:"PHP/anyadir_dato.php",
+        url:"php/envio_pelicula.php",
         type:"POST",
         data: objeto_dato, // paso el dato por el post
         dataType:"json",
     }).done(function(respuesta){
         console.log(respuesta);  // recojo la respuesta, que sera true o false
        if(respuesta){
-            alert("Dato insertado correctamente !!!!");//si es correcta, inserto los datos en una fila nueva
-            $("#lista").append("<tr><td>"+objeto_dato.nombre+"</td><td>"+objeto_dato.apellido+"</td><td>"+objeto_dato.edad+"</td></tr>");
+            $("#lista").append("<tr><td>"+objeto_dato.titulo+"</td><td>"+objeto_dato.director+"</td><td>"+objeto_dato.actor1+"</td></tr>");
+            console.log(objeto_dato.genero);
+            //alert("Dato insertado correctamente !!!!");//si es correcta, inserto los datos en una fila nueva
         }else{ 
             alert("Error en la insercion"); //si no es correcta no inserto nada
         } 
@@ -92,4 +68,42 @@ function anyadir_dato(){
     });
     ;
 
+}
+
+var total=0; // suma total de escaños
+window.onload = function(){
+    document.getElementById("partido").addEventListener("change",anyado_partido);
+    document.getElementById("limpiar").addEventListener("click",function(){
+        total=0;
+        document.getElementById("panel").innerHTML="";
+        document.getElementById("total").innerHTML="";
+    });
+}
+function anyado_partido(){
+    var options=document.getElementsByTagName("option");
+    var partido = document.createElement('span');
+    for(var i=0; i<options.length;i++){ //buscando el option seleccionado
+        if(options[i].selected){  // para recoger el atributo escanyos del elegido
+            var escanyos = parseInt(options[i].getAttribute("escanyos")); //quiero el valor de escanyos
+            console.log(escanyos);
+            partido.style.display= "inline-block"; // para poder modificar anchura
+            partido.style.width= (escanyos*2)+"px"; // ancho = escaños * 2
+            partido.textContent= options[i].text; //Texto
+            partido.style.backgroundColor=options[i].value; //color
+            partido.addEventListener("click", function(){ //gestiono evento para borrar
+                this.remove();
+                total-=escanyos;
+                document.getElementById("total").innerHTML="TOTAL :"+total;
+            } );
+            partido.addEventListener("mouseenter",function(){ //cuando entra raton
+                document.getElementById("escanyos_partido").innerHTML="Escaños :"+escanyos;
+            })
+            partido.addEventListener("mouseleave",function(){ //cuando sale raton
+                document.getElementById("escanyos_partido").innerHTML="";
+            })
+            document.getElementById("panel").appendChild(partido); // lo añado
+            total += escanyos;
+            document.getElementById("total").innerHTML="TOTAL :"+total;
+        }
+    }
 }
